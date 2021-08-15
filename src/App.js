2be35599b979b5695c09431290da0c141c02edb9
Paranya ('Anya) <80136592+anyapages@users.noreply.github.com>
2021-08-15 19:17:44 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+import Products from "./Components/Products";
+
+function App() {
+  return (
+    
+    <Products></Products>
+    
+    );
+  }
+
+export default App;
